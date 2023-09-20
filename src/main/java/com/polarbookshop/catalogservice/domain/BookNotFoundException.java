42a4f48eddf9611535c5c010e5046fa7c7efd900
Simon Gombrii <simon@gombrii.se>
2023-09-20 14:48:17 +0200

    Lägg till API

--- src/main/java/com/polarbookshop/catalogservice/domain/BookNotFoundException.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookNotFoundException.java
@@ -1,7 +1,7 @@
 package com.polarbookshop.catalogservice.domain;
 
-public class BookNotFoundException extends RuntimeException{
-    public BookNotFoundException(String isbn){
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String isbn) {
         super("The book with ISBN " + isbn + " was not found.");
     }
 }
